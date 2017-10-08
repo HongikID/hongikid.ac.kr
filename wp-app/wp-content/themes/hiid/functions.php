13bfc361282a173925b3cb8360e6ba5198529da6
Sunghyun Hwang <me@sunghyunzz.com>
2017-10-08 15:53:17 +0900

    Add functions.php

---
+++ wp-app/wp-content/themes/hiid/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+if ( ! isset( $content_width ) ) {
+    $content_width = 968;
+}
+
+if (! function_exists('hiid_theme_setup')) :
+    function hiid_theme_setup() {
+        register_nav_menus( array(
+            'header'   => __( 'Header', 'hiid' ),
+            'footer' => __( 'Footer', 'hiid' )
+        ) );
+    }
+endif;
+
+add_action( 'after_setup_theme', 'hiid_theme_setup' );
+
