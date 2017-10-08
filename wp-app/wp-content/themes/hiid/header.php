543b464dd2b086b7bc49a792753c513116f16ed4
Sunghyun Hwang <me@sunghyunzz.com>
2017-10-08 16:22:39 +0900

    Improve header to display theme menu

--- wp-app/wp-content/themes/hiid/header.php
+++ wp-app/wp-content/themes/hiid/header.php
@@ -17,41 +17,17 @@
         <a href="javascript:void(0);" id="header__nav__toggle-btn" onclick="onNavToggleButtonClicked();">메뉴 여닫기</a>
         <h1 id="header__title__logo">Hongik Industrial Design</h1>
     </div>
-    <nav id="header__nav">
-        <ul id="header__nav__list" class="container">
-            <li><a class="active" href="#">About HIID</a></li>
-            <li><a href="#">Graduate Show</a></li>
-            <li><a href="#">Alumni</a></li>
-            <li><a href="#">Contact Us</a></li>
-        </ul>
-    </nav>
+    <?php
+    wp_nav_menu(
+        array(
+            'theme_location' => 'header',
+            'menu_id' => 'header__nav__list',
+            'menu_class' => 'container',
+            'container' => 'nav',
+            'container_id' => 'header__nav'
+        )
+    );
+    ?>
 </header>
 
 <main id="container" class="container">
-    <h2>HONGIK INDUSTRIAL DESIGN</h2>
-    <p>
-        디자인학부는 다양한 문화예술 콘텐츠와 창의산업의 가치공학적 융합을 목표로 인내천(人乃天) 사상의 구현이념인 홍익인간(弘益人間)의 동시대정신을 실천하는 독창적이고도 전인적인 디자이너의 육성을 교육 이념으로 한다.  학부는 시각디자인 계열 전공과 산업디자인 계열 전공으로 구분되며, 각 세부전공별로 폭 넓은 사회문화적 이해와 예술적 경험을 바탕으로 이론적 지식과 전문의 기예를 겸비한 국제적 수준의 실무능력을 함양하기 위해 다각적인 국제교류 및 학술활동, 국내외 저명 디자이너와 문화.산업계 유관 기관과의 워크숍 및 세미나, 기획전시와 산학관 협력 프로젝트 등 다양한 실험과 연구를 통한 미래지향적 비전과 진취적인 전공 기반을 마련하고 있다.
-        디자인학부는 다양한 문화예술 콘텐츠와 창의산업의 가치공학적 융합을 목표로 인내천(人乃天) 사상의 구현이념인 홍익인간(弘益人間)의 동시대정신을 실천하는 독창적이고도 전인적인 디자이너의 육성을 교육 이념으로 한다.  학부는 시각디자인 계열 전공과 산업디자인 계열 전공으로 구분되며, 각 세부전공별로 폭 넓은 사회문화적 이해와 예술적 경험을 바탕으로 이론적 지식과 전문의 기예를 겸비한 국제적 수준의 실무능력을 함양하기 위해 다각적인 국제교류 및 학술활동, 국내외 저명 디자이너와 문화.산업계 유관 기관과의 워크숍 및 세미나, 기획전시와 산학관 협력 프로젝트 등 다양한 실험과 연구를 통한 미래지향적 비전과 진취적인 전공 기반을 마련하고 있다.
-    </p>
-
-    <h3>Sticky Navigation Example</h3>
-    <p>The navbar will stick to the top when you reach its scroll position.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p><p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p><p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p><p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-    <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
-
-
-
-
