5ec58e6626f15b624c416914a2caaa7e9921c389
Sunghyun Hwang <me@sunghyunzz.com>
2017-10-22 15:25:21 +0900

    Implement footer component

--- wp-app/wp-content/themes/hiid/footer.php
+++ wp-app/wp-content/themes/hiid/footer.php
@@ -1,5 +1,28 @@
 </main>
 
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <?php
+            wp_nav_menu(
+                array(
+                    'theme_location'    => 'footer',
+                    'container'         => 'div',
+                    'container_class'   => 'footer__menu col-sm-12 col-md-2',
+                    'menu_class'        => 'footer__menu__list',
+                    'depth'             => 0,
+                )
+            );
+            ?>
+
+            <div class="footer__address col-sm-12 col-md-10">
+                <strong class="footer__address__subject">홍익대학교 산업디자인과</strong>
+                <address>서울시 마포구 와우산로 94 홍익대학교 조형관 904호 | <a href="tel:+8223201215">02 320 1215</a> | <a href="mailto:hiid@hongik.ac.kr">hiid@hongik.ac.kr</a></address>
+            </div>
+        </div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
